567062983f078fc7d2b1571173be92659d410a01
Victor marshall911 <Victormarshall911>
2023-06-17 16:05:12 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+printf("Size of char: %zu byte(s)\n", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
+printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+
+    return 0;
+}
+
