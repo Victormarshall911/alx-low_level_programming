98c89f26d70f9643603004989891c8d4ebcb8a1a
Victor marshall911 <Victormarshall911>
2023-06-29 11:54:33 -0700

    stree

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,23 @@
 #include "main.h"
 /**
- * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
  *
- * Return: dest
+ * Return: s1[i] - s2[i]
  */
-char *_strncpy(char *dest, char *src, int n)
+int _strcmp(char *s1, char *s2)
 {
-	int _strcmp(char *s1, char *s2) {
-			while (*s1 && (*s1 == *s2)) {
-				s1++;
-				s2++;
-}
+	int i;
 
-		return *(unsigned char *)s1 - *(unsigned char *)s2;
-}
-
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (0);
 }
