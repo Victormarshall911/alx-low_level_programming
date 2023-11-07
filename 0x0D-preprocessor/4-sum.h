ff785f3ee6a027f9c97a1cc8a68099be6eae7a66
Victormarshall911 <favourjames1977@gmail.com>
2023-11-07 15:21:27 -0800

    preprocessorr

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SUM(x, y) ((x) + (y))
+
+int main() {
+		int a = 5;
+		int b = 7;
+		int result = SUM(a, b);
+
+		printf("The sum of %d and %d is %d\n", a, b, result);
+
+		return 0;
+}
+
